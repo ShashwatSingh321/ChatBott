7fce576317dc8f7445c84422292393ef2ab84e2a
ShashwatSingh321 <shashwatsingh73557@gmail.com>
2024-12-14 00:24:59 +0530

    Update db.js

--- database/db.js
+++ database/db.js
@@ -4,6 +4,8 @@ const connectDb = async () => {
   try {
     await mongoose.connect(process.env.Db_url, {
       dbName: "ChatbotYoutube",
+      connectTimeoutMS: 30000,  // Increase timeout to 30 seconds
+    socketTimeoutMS: 30000 
     });
 
     console.log("Mongo db connected");
